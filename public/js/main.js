d12136fd9349d9753d8708ce7602d24f385e3559
Adam Moore <amooreinteractive@gmail.com>
2015-02-07 12:03:54 -0800

    routing working, category pages, regular pages, and home page

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 //nb-wiki/main.js
 
-var app = angular.module('ng-wiki', ['ngRoute']);
+var app = angular.module('ng-wiki', ['ngRoute', 'ngSanitize']);
 
     // configure our routes
     app.config(function($routeProvider) {
@@ -12,7 +12,13 @@ var app = angular.module('ng-wiki', ['ngRoute']);
             })
 
             // route for the about page
-            .when('/categories/:catName', {
+            .when('/categories/:catUrl', {
+                templateUrl : 'pages/category.html',
+                controller: 'catCtrlr'
+            })
+
+            // route for the about page
+            .when('/pages/:pageName', {
                 templateUrl : 'pages/page.html',
                 controller: 'pageCtrlr'
             })
@@ -22,31 +28,33 @@ var app = angular.module('ng-wiki', ['ngRoute']);
     });
 
 app.run(function($rootScope){
-    $rootScope.name = "Online Production Wiki 2015";
+    $rootScope.name = "ng-Wiki (WIP)";
     
     //Dummy Data - Need to pull this data from the DB when ready.
-    $rootScope.user = 'Adam Moore';
+    $rootScope.user = 'amoore';
     
     //Dummy Data for Pages and Categories all in one Object
     $rootScope.wiki = {
-        categories: [  {name: 'Internal Docs',    url: 'internal-docs',      parent: null}, 
-                        {name: 'Agency Docs',     url: 'agency-docs',      parent: null}, 
-                        {name: 'Reference Docs',  url: 'ref-docs',      parent: null}, 
-                        {name: 'Content Guides',  url: 'content-guides',      parent: null},
-                        {name: 'Wiki Tools',      url: 'wiki-tools',      parent: null}
+        categories: [  {name: 'Internal Docs',    url: 'internal-docs'}, 
+                        {name: 'Agency Docs',     url: 'agency-docs'}, 
+                        {name: 'Reference Docs',  url: 'ref-docs'}, 
+                        {name: 'Content Guides',  url: 'content-guides'},
+                        {name: 'ngWiki Tools',      url: 'wiki-tools'}
                      ],
         pages: [{  name:'Home', 
-                    category: null, 
+                    category: null,
+                    title: 'Home', 
                     author: $rootScope.user, 
                     body: '',
                   },
                   {  name:'Categories', 
-                    category: 'Wiki Tools', 
+                    category: 'ngWiki Tools',
+                    title:'Categories', 
                     author: $rootScope.user, 
-                    body: '',
+                    body: 'A List of the categories that have been defined.',
                   }                  
                 ]
-    }
+    };
     
     $rootScope.emptyPage = {
         author: null,
@@ -55,22 +63,36 @@ app.run(function($rootScope){
     };
 });
 
-app.controller('pageCtrlr', function($scope, $routeParams){
-    $scope.page = angular.copy($scope.emptyPage);
-    $scope.page.name = $routeParams.catName;
-    $scope.showEdit = function(){
-        $('#article-edit').show();
-        $scope.page.author = $scope.user;
-    };
-});
-
 app.controller('homeCtrlr', function($scope){
     $scope.motd = 'Welcome to ngWiki!';
 });
 
+app.controller('catCtrlr', function($scope, $routeParams){
+    var routedCat = $routeParams.catUrl;
+    angular.forEach($scope.wiki.categories, function(val, key){
+        if(val.url == routedCat){
+            $scope.cat = val;
+        }
+    });
+});
+
+app.controller('pageCtrlr', function($scope, $routeParams){
+    var routedTitle = $routeParams.pageName;
+    angular.forEach($scope.wiki.pages, function(val, key){
+        if(val.title == routedTitle){
+            $scope.page = val;
+        }
+    });
+    //$scope.page = currPage;
+
+    $scope.showEdit = function(){
+        $('#article-edit').show();
+    };
+});
+
 app.controller('inputCtrlr', function($scope){
     $scope.updateArticle = function(page){
-        $scope.page = page; //angular.copy(page);
+        $scope.page = page;
         $('#article-edit').hide();
     };
     $scope.clearArticle = function(){
