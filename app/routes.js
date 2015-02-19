28dcdfac777255ac700596beda54e4ba6f56d205
Adam Moore <amooreinteractive@gmail.com>
2015-02-18 16:57:15 -0800

    cleaning

--- app/routes.js
+++ app/routes.js
@@ -6,30 +6,28 @@ var User = require('./models/user');
 
     module.exports = function(app) {
 
-        // server routes ===========================================================
-
+        // server routes ===========================================================        
         // sample api route
-        app.get('/api/categories', function(req, res, next) {
+        app.get('/api/categories', function(req, res) {
             // use mongoose to get all nerds in the database
             Category.find(function(err, cats) {
-                if(err){ return next(err); }
-
+                if(err){ res.send(err); }
                 res.json(cats); // return all nerds in JSON format
             });
         });
 
         // route to handle creating goes here (app.post)
-        app.post('/api/categories', function(req, res, next) {
+        /*app.post('/api/categories', function(req, res, next) {
 
             var cat = new Category(req.body);
 
               cat.save(function(err, cat){
-                if(err){ return next(err); }
+                if(err){ res.send(err); }
 
                 res.json(cat);
             });
 
-        });
+        });*/
         // route to handle delete goes here (app.delete)
 
         // frontend routes =========================================================
